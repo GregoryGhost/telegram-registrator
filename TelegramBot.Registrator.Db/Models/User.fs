fed8e5976d89d0e3e2cd37d353035a52a57eb06a
Gregory <KulakovGV@tomskasu.ru>
2018-11-25 22:08:50 +0700

    Добавил модель пользовательских данных.

---
+++ TelegramBot.Registrator.Db/Models/User.fs
@@ -0,0 +1,16 @@
+﻿namespace TelegramBot.Registrator.Db.Models
+
+open System
+
+/// Модель данных о пользователе.
+type User =
+    {
+        /// Фамилия.
+        SurName: string
+        /// Имя.
+        Name: string
+        /// Отчество.
+        Patronymic: string
+        /// Дата рождения.
+        DateOfBirth: DateTime
+    }
