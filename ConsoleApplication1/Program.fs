0090d5fdff122552cf87d52347ea25fe40fb5112
Gregory <KulakovGV@tomskasu.ru>
2019-01-12 23:59:08 +0700

    Добавил тестовый консольный проект для проверки работы БД сборки телеграмм бота: - подключения к БД; - накатывание миграций; - получение данных из БД; - запись данных в БД.

---
+++ ConsoleApplication1/Program.fs
@@ -0,0 +1,17 @@
+﻿open TelegramBot.Registrator.Db
+open TelegramBot.Registrator.Db.Models
+open TelegramBot.Registrator.Db.Services
+open System
+
+[<EntryPoint>]
+let main argv = 
+    Migrator.run
+    let us = new UserService()
+    let newUser = {
+        SurName = "Yugami1234";
+        Name = "Lite";
+        Patronymic = "Kira";
+        DateOfBirth = DateTime.Now    }
+    us.Registrate newUser |> ignore
+    us.PrintAllUsers
+    0 // return an integer exit code
