1c54ca77927becd75478040a2440b73e079e0fc4
Gregory <KulakovGV@tomskasu.ru>
2019-01-27 20:09:49 +0700

    Добавил тест по проверке создания пользователя после его удаления. Добавил также todo'шек =).

--- TelegramBot.Regitstrator.IntegrationTests/Tests/UserServiceTest.fs
+++ TelegramBot.Regitstrator.IntegrationTests/Tests/UserServiceTest.fs
@@ -7,6 +7,8 @@ module UserServiceTest =
     open System
 
     let tests idTelegramUser =
+        // TODO: вынести повторяющиеся шаги в функции вспомогательного модуля
+        //  и сделать в них проверку на выполнение шага
         let newUser = {
             SurName = "Yagami"
             Name = "Lite"
@@ -15,6 +17,7 @@ module UserServiceTest =
         }
         testList "Проверка CRUD UserService" [
             test "Добавление пользователя" {
+                // TODO: сделать сервис IDisposible
                 let us = new UserService()
                 us.Registrate(idTelegramUser, newUser) |> ignore
                 let gotUser =
@@ -24,6 +27,7 @@ module UserServiceTest =
                 Expect.equal gotUser newUser "Получить вновь созданного пользователя для idTelegramUser"
             }
             test "Добавление дублирующегося пользователя" {
+                // TODO: сделать сервис IDisposible
                 let us = new UserService()
                 let successRegistration = us.Registrate(idTelegramUser, newUser)
                 Expect.isTrue successRegistration "Пользователь не зарегистрирован"
@@ -33,11 +37,24 @@ module UserServiceTest =
                 Expect.isFalse failRegisration "Отказ в регистрации дублирующегося пользователя"
             }
             test "Удаление пользователя" {
+                // TODO: сделать сервис IDisposible
                 let us = new UserService()
                 us.Registrate(idTelegramUser, newUser) |> ignore
                 us.Remove(idTelegramUser)
                 let gotUser = 
                     us.GetUserById(idTelegramUser)
                 Expect.isNone gotUser "Пользователь удален"
+            }
+            test "Создание пользователя после его удаления" {
+                // TODO: сделать сервис IDisposible
+                let us = new UserService()
+                us.Registrate(idTelegramUser, newUser) |> ignore
+                us.Remove(idTelegramUser)
+                us.Registrate(idTelegramUser, newUser) |> ignore
+                let gotUser =
+                    us.GetUserById(idTelegramUser)
+                    |> Option.get
+                us.Remove(idTelegramUser)
+                Expect.equal gotUser newUser "Пользователь создан"
             }
         ]
